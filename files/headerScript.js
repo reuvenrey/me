4bdf93558500dbdd5576da77eb283f4e6deea42f
Reuven Reyman <94356206+reuvenrey@users.noreply.github.com>
2023-12-04 12:35:08 -0800

    Update headerScript.js

--- files/headerScript.js
+++ files/headerScript.js
@@ -1,9 +1,12 @@
 const loader = document.getElementById("loader");
 
 const footer = document.getElementById("footer");
+const header = document.getElementById("header");
 
 function runHeader(){
   console.log("Creating Header and Footer");
+  header.innerHTML = "";
+  header.innerHTML += "<div id='headerBar'> <img src='https://reuvenrey.github.io/me/files/images/favicon.ico'> <a href=''>Contact</a> <a href=''>Projects</a> <a href=''>Experience</a> <a href=''>Skills</a> </div>";
 }
 
 function finishLoad(){
@@ -11,3 +14,5 @@ function finishLoad(){
   loader.style.opacity = 0;
   setInterval(function () {loader.style.display = 'none'}, 500);
 }
+
+//<div id='headerSpacer'></div> <div id='headerBar'><img id='headerLogo' src='https://reuvenrey.github.io/me/files/images/favicon.ico'> <a href=''>Link 1</a></div>
