9cae1e6dcc2d9e77d41ccf40ab6053ada6eb9d60
Reuven Reyman <94356206+reuvenrey@users.noreply.github.com>
2023-12-06 09:39:04 -0800

    Update headerScript.js

--- files/headerScript.js
+++ files/headerScript.js
@@ -10,15 +10,24 @@ const link3 = linkHead + "education";
 const link4 = linkHead + "contact";
 
 var screenWidth = screen.width;
+window.onresize = resizeHeader();
 
 function runHeader(){
   console.log("Creating Header and Footer");
   header.innerHTML = "<div id='headerSpacer'></div>";
-  if(screenWidth >= 700){
-  header.innerHTML += "<div id='headerBar'> <div id='headerBox'> <img id='headerLogo' src='https://reuvenrey.github.io/me/files/images/favicon.ico' onclick='window.open("+'"'+linkHead+'","_self"'+")'> <a href='"+link4+"'>Contact</a> <a href='"+link2+"'>Projects</a> <a href='"+link1+"'>Experience</a> <a href='"+link3+"'>Education</a> </div> </div>";
+  if(screenWidth >= 800){
+    console.log("Creating large header");
+    header.innerHTML += "<div id='headerBar'> <div id='headerBox'> <img id='headerLogo' src='https://reuvenrey.github.io/me/files/images/favicon.ico' onclick='window.open("+'"'+linkHead+'","_self"'+")'> <a href='"+link4+"'>Contact</a> <a href='"+link2+"'>Projects</a> <a href='"+link1+"'>Experience</a> <a href='"+link3+"'>Education</a> </div> </div>";
   }else{
     console.log("Creating Mini-Header");
     header.innerHTML += "<div id='headerBar'><img id='headerLogo' src='https://reuvenrey.github.io/me/files/images/favicon.ico' onclick='window.open("+'"'+linkHead+'","_self"'+")'> <a>&#8681;</a> </div>";
+  }
+} //End of function runHeader()
+
+function resizeHeader(){
+  if(screen.width != screenWidth){
+    runHeader();
+    console.log("Resizing Screen");
   }
 }
 
