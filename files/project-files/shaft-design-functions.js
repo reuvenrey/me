a658502a14944cef0c2be34631144bbd2eba4dc3
Reuven Reyman <94356206+reuvenrey@users.noreply.github.com>
2024-03-17 09:54:06 -0700

    Update shaft-design-functions.js

--- files/project-files/shaft-design-functions.js
+++ files/project-files/shaft-design-functions.js
@@ -3,6 +3,30 @@
 shaftDiameterIN = [0.25, 0.5, 0.75, 0.875, 1, 1.125, 1.25, 1.375, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 3.25, 3.5, 3.75, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5,8];
 shaftDiameterMM = [6.35, 12.7, 19.05, 22.23, 25.4, 28.58, 31.75, 34.93, 38.1, 44.45, 50.8, 57.15, 63.5, 69.85, 76.2, 82.55, 88.9, 95.25, 101.6, 114.3, 127, 139.7, 152.4, 165.1, 177.8, 190.5,203.2];
 
+const ui = document.getElementById("projConsole"); // console: 	ui.innerHTML += "";
+
 function fsTestInitial(){
   console.warn("Function Script Loaded!");
 }
+
+// GUESS THE DIAMERTER USING DE GOODMAN
+function guessDiameter(){
+
+	  ui.innerHTML += "<p><em>Guessing a new diameter d (DE-Goodman)</em></p>";
+	    
+	  var d_new = 0;
+	
+	  var tempVar = 4*(Math.pow((Kf*momentA),2)) + 3*(Math.pow((Kfs*torqueA),2));
+	  let Aval = Math.sqrt(tempVar);
+	
+	  tempVar = 4*(Math.pow((Kf*momentM),2)) + 3*(Math.pow((Kfs*torqueM),2));
+	  let Bval = Math.sqrt(tempVar);
+	  
+	   ui.innerHTML += "<p style='margin-left:40px;'>A Value: " + Aval + ", B Value: " + Bval + "</p>";
+
+		tempVar = ((16*fos) / Math.pi)*((Aval / Se) + (Bval / Su));
+
+		d_new = Math.pow(tempVar, (1/3));
+			
+  return d_new;
+}// returns new guess for diameter d (unscaled)
